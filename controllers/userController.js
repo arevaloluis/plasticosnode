b98ec1d09d9f2426505e2940b72afef33aaab9a6
arevaloluis <arvaloelbaby@gmail.com>
2022-05-10 23:12:22 -0500

    agregue vista de user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const userController = {};
 
 userController.index = (request, response) => {//es una funcion de flecha
 
-    response.render('usuarios/index'); //200 que esta bien  //500 error de la base de datos*/
+    response.render('users/index'); //200 que esta bien  //500 error de la base de datos*/
 }
 
 module.exports = userController; 
